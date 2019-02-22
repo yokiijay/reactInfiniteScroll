f279ee8c5bfc972988139753d3b3f00135b06e7b
Yokiijay <yokiijay@qq.com>
2019-02-22 11:00:37 +0800

    Completed the challenge!

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const config = require('universal-config')
 const Unsplash = require('unsplash-js').default
 const toJson = require('unsplash-js').toJson
 const express= require('express')
+const app = express()
 
+//配置unsplash
 const unsplash = new Unsplash({
 	applicationId: config.get('APPLICATION_ID'),
 	secret: config.get('SECRET'),
 	callbackUrl: config.get('CALLBACK_URL')
 })
 
-const app = express()
 
 app.get('/api/photos', (req,res)=>{
 	unsplash.photos.listPhotos(req.query.start, req.query.count)
--- client/src/Components/Images/index.js
+++ client/src/Components/Images/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import axios from 'axios'
+import uuidv4 from 'uuid/v4'
 
 //Components
 import InfiniteScroll from 'react-infinite-scroll-component'
@@ -7,13 +8,14 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 class Images extends Component {
 	state = {
 		images: [],
-		count: 30,
-		start: 1
+		count: 24,
+		start: 1,
+		hasMore: true
 	}
 
 	componentDidMount(){
 		const { count, start } = this.state
-		axios.get(`/api/photos?count=${count}&start=&${start}`)
+		axios.get(`/api/photos?count=${count}&start=${start}`)
 		.then(res => {
 			this.setState((prevState)=>(
 				{
@@ -24,11 +26,44 @@ class Images extends Component {
 		.catch(err => {console.log(err)})
 	}
 
+	fetchImages= ()=>{
+		// 获取新的一页
+		this.setState((prevState)=>(
+			{
+				start: prevState.start + 1,
+				count: 6
+			}
+		),()=>{ // 异步setState后axios请求新的一页数据然后更新state
+			axios.get(`/api/photos?count=${this.state.count}&start=${this.state.start}`)
+			.then(res => {
+				this.setState((prevState)=>(
+					{
+						images: prevState.images.concat(res.data)
+					}
+				))
+			})
+			.catch(err => {console.log(err)})
+		})
+	}
+
 	render(){
 		console.log(this.state)
 		return(
 			<Fragment>
-				Images
+				<InfiniteScroll
+				  dataLength={this.state.images.length} //This is important field to render the next data
+				  next={this.fetchImages}
+				  hasMore={this.state.hasMore}
+				  loader={<h4>Loading...</h4>}
+				  endMessage={
+				    <p style={{textAlign: 'center'}}>
+				      <b>Yay! You have seen it all</b>
+				    </p>
+				  }>
+				  { this.state.images.map((image,index)=>(
+				  	<img key={uuidv4()} className="single-photo" src={image.urls.thumb} alt=""/>
+				  )) }
+				</InfiniteScroll>
 			</Fragment>
 		)
 	}
