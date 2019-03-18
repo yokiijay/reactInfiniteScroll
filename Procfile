5415b99ad869f3facd54639f52ee2bb0b960bb40
Xmac <xmaczone@Xmac.local>
2019-03-19 00:09:05 +0800

    2019-03-19 00:09:04

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: yarn dev set PORT=$PORT
--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.get('/api/photos', (req,res)=>{
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>console.log(`Server started on PORT ${PORT}`))
+app.listen(5000, ()=>console.log(`Server started on PORT ${PORT}`))
